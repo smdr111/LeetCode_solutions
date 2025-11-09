7982477aba7b06b3a7a0b4ed5d6784b3d554e98d
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-11-09 01:37:36 -0500

    Add function to calculate special bonus for employees

---
+++ Pandas/calculate_special_bonus.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'].loc[(employees['name'].str[0]=='M')|(employees['employee_id']%2==0)]=0
+    employees['bonus']=employees['salary'].copy()
+    return employees[['employee_id','bonus']].sort_values(by='employee_id')

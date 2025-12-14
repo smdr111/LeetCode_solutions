95a97ec92480ee8b406f0b76a08c82ccff3f1448
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2025-12-14 15:20:15 -0500

    Add function to replace employee IDs with names

---
+++ Pandas/replace_employee_id.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    result = employees.merge(employee_uni,how='left')
+    return result[['unique_id','name']]
+    

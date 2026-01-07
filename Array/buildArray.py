d224d5f15f66780570c758159975d0d11dc1a775
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-01-07 10:56:21 -0500

    Implement buildArray method in Solution class

---
+++ Array/buildArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in nums:
+            result.append(nums[i])
+        return result
+        

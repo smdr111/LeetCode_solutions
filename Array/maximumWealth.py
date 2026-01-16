d404156b9ba70c1f1272fc8a580d97e9b0d7e224
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-01-16 15:33:38 -0500

    Implement maximumWealth function in Solution class

---
+++ Array/maximumWealth.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        result = []
+        for i in accounts:
+            result.append(sum(i))
+        return max(result)
+        

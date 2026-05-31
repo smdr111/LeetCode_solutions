cf99ce952e60902ca33c50058a976f23fe83e870
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-05-31 15:45:10 -0400

    Implement asteroidsDestroyed method in Solution class

---
+++ Array/asteroidsDestroyed.py
@@ -0,0 +1,10 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for i in asteroids:
+            if i>mass:
+                return False
+            mass += i
+        return True
+
+        

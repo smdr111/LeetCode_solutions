f40852504c46a620b4920fca907a546bd5cb763d
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-06-02 12:18:20 -0400

    Add earliestFinishTime method in Solution class
    Implement earliestFinishTime method to calculate the earliest finish time based on land and water schedules.

---
+++ Array/earliestFinishTime.py
@@ -0,0 +1,18 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        res = inf
+        n = len(landStartTime)
+        m = len(waterStartTime)
+        for i in range(n):
+            for j in range(m):
+                land = landStartTime[i] + landDuration[i]
+                land_water = max(land,waterStartTime[j]) + waterDuration[j]
+                res = min(res,land_water)
+
+                water = waterStartTime[j] + waterDuration[j]
+                water_land = max(water,landStartTime[i]) + landDuration[i]
+                res = min(res,water_land)
+        return res
+
+
+        
